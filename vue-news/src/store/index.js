06ec5d340974562008b92726625906088c4346d3
vinocturne <vinocturne@naver.com>
2021-05-19 17:24:12 +0900

    vuex modularization

--- vue-news/src/store/index.js
+++ vue-news/src/store/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import { fetchAskList, fetchJobsList, fetchNewsList } from '../api/index.js';
+//모듈화
+import mutations from './mutations.js';
+import actions from './actions.js';
 
 Vue.use(Vuex);
 
@@ -10,51 +12,11 @@ export const store = new Vuex.Store({
     asks : [],
     jobs : []
   },
-  mutations: {
-      //state로 데이터를 넘기기 때문에 첫 인자는 무조건 state로 해야한다.
-    SET_NEWS(state, news) {
-        state.news = news;
-    },
-    SET_ASKS(state, asks) {
-        state.asks = asks;
-    },
-    SET_JOBS(state, jobs) {
-        state.jobs = jobs;
+  getters: {
+    fetchedAsk(state) {
+        return state.asks;
     }
   },
-  actions : {
-      FETCH_NEWS(context) {
-        fetchNewsList()
-        .then(response => {
-            console.log(response);
-            //구조상 actions에서 state로 바로 데이터를 바인딩 할 수 없다.
-            //actions에서는 mutations을 거쳐 state로 가기 때문에
-            //mutations에서 이를 담아주는 함수를 실행해야함.
-            //actions에서는 mutations에 접근할 수 있게 context가 제공된다.
-            //context.commit으로 해당 mutations에 접근할 수 있다.
-            context.commit('SET_NEWS', response.data);
-        })
-        .catch(error => {
-            console.log(error);
-        });
-      },
-      FETCH_ASKS(context) {
-          fetchAskList()
-          .then(response => {
-              context.commit('SET_ASKS', response.data);
-          })
-          .catch(error => {
-              console.log(error);
-          })
-      },
-      FETCH_JOBS(context) {
-          fetchJobsList()
-          .then(response => {
-              context.commit('SET_JOBS', response.data);
-          })
-          .catch(error => {
-              console.log(error);
-          })
-      }
-  }
+  mutations,
+  actions
 });
